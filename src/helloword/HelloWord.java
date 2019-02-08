e3826aecb09ee1157e3c28afaed2897082d63509
guang <840070718@qq.com>
2019-02-08 16:51:07 +0800

    update

--- src/helloword/HelloWord.java
+++ src/helloword/HelloWord.java
@@ -4,6 +4,6 @@ public class HelloWord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Word 随便改一改");	}
+		System.out.println("Hello Word 进行一些更改");	}
 
 }
